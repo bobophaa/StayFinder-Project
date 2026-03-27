32675fcee0a598bb8f8816d820f0852b003e79a0
sieitha15 <sieitha4@gmail.com>
2026-03-27 14:12:52 +0700

    pull from bopha

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
 import 'bootstrap-icons/font/bootstrap-icons.css'
+
+//toast
+import 'vue-toast-notification/dist/theme-sugar.css'
+
 import App from './App.vue'
 import router from './router'
 
