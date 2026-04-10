22d25bed8b8788d0af97073772b89126b1f3e7da
bobophaa <hingchanbopha4@gmail.com>
2026-04-10 11:19:43 +0700

    fix url

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: import.meta.env.VITE_API_URL + '/api'
+  baseURL: 'https://bontubjoul2.csm.linkpc.net/api', 
 });
 
 api.interceptors.request.use(
